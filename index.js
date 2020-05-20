21bb38aee48d479c72ee5315a67c4e43e21061b8
sujitbidawe <bidawesujit@gmail.com>
2020-05-20 11:56:16 +0530

    added: code for arrow functions

--- index.js
+++ index.js
@@ -44,3 +44,26 @@ person.walk();
 const walk = person.walk.bind(person);  // bind method sets the value of this permanently
 walk(); // gives person object without calling from person object since we bind the value of person with bind
 
+// traditional way of declarinf functions
+// const square = function(number) {
+//     return number * number;
+// };
+
+// use arrow functions for more compact code. if we have only one statement in body, need noot use return keyword
+// if no parameters required, declare function with empty parentheses. no parantheses needed if single parameter
+const square = number => number * number; // this is same as line number 47 to 50
+
+console.log(square(5));
+
+const jobs = [
+    {id: 1, isActive: true},
+    {id: 2, isActive: true},
+    {id: 3, isActive: false}
+]
+
+// const activeJobs = jobs.filter(function(job) {
+//     return job.isActive;
+// });
+
+const activeJobs = jobs.filter (job => job.isActive); // this is same as line number 64 to 66
+console.log(activeJobs);
