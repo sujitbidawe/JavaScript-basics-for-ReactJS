41c8cba2fb92574b87e3616762e7abf49e217b79
sujitbidawe <bidawesujit@gmail.com>
2020-05-20 10:26:23 +0530

    added: code for . operator and square operator method for accessing object properties

--- index.js
+++ index.js
@@ -19,4 +19,20 @@ sayHelloWithLet();
 
 // const is immutable variable
 const x = 5;
-x = 1; // this reassignment not allowed, throws error
+//x = 1; // this reassignment not allowed, throws error
+
+// person object with 3 members. 1 property and 2 methods
+const person = {
+    name: 'foo',
+    walk: function() {},
+    talk() {}
+};
+
+person.talk(); // dot notation way for accessing object
+person['name'] = "bar"; // this method is used when we dont know which prorety or method is going to be used of the object
+
+const targetMember = 'name';
+person[targetMember] = 'sujit'; // when we dont know beforehand which property or method of an object we are going to access then we use
+// this method
+
+console.log(person.name);
